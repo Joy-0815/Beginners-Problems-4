c1d3f74fef3b175a3a97429726ee669c06253863
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-21 18:47:22 +1200

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
-prime = int(input("Input a prime number."))
-if prime == 2 or prime == 3 or prime == 5 or prime ==  7 or prime == 11 or prime == 13 or prime == 17 or prime == 19 or prime == 23 or prime == 29 or prime == 31 or prime == 37 or prime == 41 or prime == 43 or prime == 47  or prime == 53 or prime == 59 or prime == 61 or prime == 67 or prime == 71 or prime == 73 or prime == 79 or prime == 83 or prime == 89 or prime == 97 or prime == 101:
+prime = int(input("Input a number."))
+if prime in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 91, 101]
     print("It is a prime number.")
 else:
     print("It is a not a prime number.")
